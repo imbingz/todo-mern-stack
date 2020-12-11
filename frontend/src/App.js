13bca7237b49b3bc2ba96a9ae2b6db83c00c4c2e
Bing Z <bingz@me.com>
2020-12-10 20:15:01 -0700

    initial routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<h1>Todo App</h1>
+			<Switch>
+				<Route exact path='/'  component={} />
+				<Route exact path='/todos'  component={} />
+				<Route exact path='/todos/new'  component={} />	
+			</Switch>
+		</BrowserRouter>
+	);
 }
 
 export default App;

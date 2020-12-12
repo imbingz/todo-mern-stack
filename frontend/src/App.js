31f911252e7fec002d1c1792180b1bf2029ccb2a
Bing Z <bingz@me.com>
2020-12-11 23:38:03 -0700

    wrap routes inside GlobalProvider

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import GlobalProvider from './utils/GlobalContext';
 import AddTodo from './pages/AddTodo';
 import Homepage from './pages/Homepage';
 import ViewTodos from './pages/ViewTodos';
+import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Switch>
-				<Route exact path='/' component={Homepage} />
-				<Route exact path='/todos' component={ViewTodos} />
-				<Route exact path='/todos/new' component={AddTodo} />
-			</Switch>
+			<GlobalProvider>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={Homepage} />
+					<Route exact path='/todos' component={ViewTodos} />
+					<Route exact path='/todos/new' component={AddTodo} />
+				</Switch>
+			</GlobalProvider>
 		</BrowserRouter>
 	);
 }

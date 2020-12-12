9e00a4f9855e4d3211fe289a69f58df0de995b44
Bing Z <bingz@me.com>
2020-12-12 01:02:44 -0700

    added extra scripts and condition for heroku dploy

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 const app = express();
+const path = require('path');
 
 //db connection
 require('./config/db')();
@@ -11,6 +12,14 @@ const PORT = process.env.PORT || 5005;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
+
+//For heroku deployment
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../client/build/index.html'));
+	});
+}
 
 app.listen(PORT, () => {
 	console.log(`listening on PORT ${PORT}. http://localhost:${PORT}`);

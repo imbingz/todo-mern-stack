1f5c9132dc577f67483761b1935b07c29dbc5b59
Bing Z <bingz@me.com>
2020-12-12 01:00:03 -0700

    added useContext n useReducer

--- frontend/src/pages/ViewTodos.js
+++ frontend/src/pages/ViewTodos.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useGlobalContext } from '../utils/GlobalContext';
 
 function ViewTodos() {
-	return <div>view todos</div>;
+	//this is what we setup in  GlocalContext.js
+	const [ state, dispatch ] = useGlobalContext();
+
+	console.log('viewtodo state from useGlbalContext: ', state);
+
+	useEffect(() => {
+		async function fetchTodos() {
+			try {
+				const response = await fetch('/api/todo');
+				const json = await response.json();
+				// console.log(json.data);
+				//==> (json.data) is array of todo obj {_id: , text: ""}, json obj is what we sent out as response from backend {success: true, data}
+				//pass arguments to dispacth
+				dispatch({ tpye: 'setTodos', payload: json.data });
+			} catch (err) {
+				console.error(err);
+			}
+		}
+		fetchTodos();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<div className='Todos'>
+			<h3 className='Todos-header'>Current Todos</h3>
+			<ul className='Todos-list'>
+				{/* Todo list goes here. */}
+				{state.todos.map(todo => (
+					<li key={todo._id} className='Todos-listItem'>
+						<span>{todo.text}</span>
+						<button onClick={() => console.log('button clicked, delete todo later')}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
 }
 
 export default ViewTodos;

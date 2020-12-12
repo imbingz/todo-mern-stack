def875676bed53f7a3a25839589adb449ebb885d
Bing Z <bingz@me.com>
2020-12-12 00:01:02 -0700

    setup globalcontext util

---
+++ frontend/src/utils/GlobalContext.js
@@ -0,0 +1,39 @@
+import { useContext, useReducer, createContext } from 'react';
+
+//setup  global context
+export const GlobalContext = createContext();
+
+// set  up  global provider and reducer
+const initState = {
+	todos: []
+};
+
+const reducer = (state, action) => {
+	console.log(state, action);
+	switch (action.type) {
+		case 'setTodos':
+			return {
+				...state,
+				todos: action.payload
+			};
+		default:
+			return state;
+	}
+};
+
+const GlobalProvider = props => {
+	const [ state, dispatch ] = useReducer(reducer, initState);
+	return <GlobalContext.Provider value={[ state, dispatch ]} {...props} />;
+};
+
+//or we can explicitly use props.children
+// const GlobalProvider = props => {
+// 	return <GlobalContext.Provider value={{ a: 'hello', b: 'world' }}>{props.children}</GlobalContext.Provider>;
+// };
+
+//export GlobalProvider
+export default GlobalProvider;
+
+export const useGlobalContext = () => {
+	return useContext(GlobalContext);
+};
